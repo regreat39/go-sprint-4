332da26fc774c8de015b0185602d5c73abdb8b96
Александр Галицкий <galitskiy.sashul@mail.ru>
2026-07-24 22:44:37 +0200

    second commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -60,6 +60,6 @@ func DayActionInfo(data string, weight, height float64) string {
 		return ""
 	}
 
-	result := fmt.Sprintf("Количество шагов: %d\nДистанция составила %.2f км\nВы сожгли %.2f ккал\n", step, distanceKm, calories)
+	result := fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", step, distanceKm, calories)
 	return result
 }
